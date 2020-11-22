8f19973e63428a983cf4009ba8b358831e79570a
JeandreMitton <42011760+JeandreMitton@users.noreply.github.com>
2020-11-22 14:18:22 +0200

    added express again

--- frontman/public/script.jsx
+++ frontman/public/script.jsx
@@ -1,9 +1,30 @@
 var Signup = React.createClass({
     render: function() {
         return(
-            <h2>What's up React</h2>
+            <div>
+                <header class="showcase">
+                    <div class="container showcase-inner">
+                        <h1>Welcome to Mitton's Member Management</h1>
+                        <p>Please login or sign up for a membership</p><br></br>
+                        <p>Email: <input id = "email"/></p><br></br>
+                        <p>Password: <input id = "password"/></p>
+                        <button href="display.html" class="btn">Submit</button>
+                        <a href="sub.html" class="btn">Sign Up</a>
+                    </div>
+                </header>
+            </div>
         );
-    }
+    },
+    fetch(uri)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log( data );
+    })
+    .catch(function(err){
+        //error handling network request
+    })
 });
 
 React.render(<Signup/>, document.getElementById("root"));
--- frontman/src/App.js
+++ frontman/src/App.js
@@ -3,24 +3,16 @@
 function App() {
   return (
     <div>
-      <section>
-      <header className='container'>
-        <div style={{display: 'flex',flexDirection: 'column',alignItems: 'center',justifyContent: 'center',textAlign: 'center',height: '100%',opacity: 1,transition:' opacity 1s ease-in'}} className="container showcase-inner">
-            <h1>Welcome to Mitton's Member Management</h1>
-            <p>Please login or sign up for a membership</p><br></br>
-            <form>
-            <p>
-                Email: <input id = "email"/>
-            </p><br></br>
-            <p>
-                Password: <input id = "password"/>
-            </p>
-            <button href="display.html" className="btn">Submit</button>
-            <a href="sub.html" className="btn">Sign Up</a>
-            </form>
-            </div>
-        </header>
-        </section>
+      <header class="showcase">
+        <div class="container showcase-inner">
+          <h1>Welcome to Mitton's Member Management</h1>
+          <p>Please login or sign up for a membership</p><br></br>
+          <p>Email: <input id = "email"/></p><br></br>
+          <p>Password: <input id = "password"/></p>
+          <button href="display.html" class="btn">Submit</button>
+          <a href="sub.html" class="btn">Sign Up</a>
+        </div>
+      </header>
     </div>
   );
 }
